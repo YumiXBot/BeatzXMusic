5465004347145b859eae3682e3a1b41d198eb27c
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2024-02-07 21:43:19 +0530

    Update help.py

--- DAXXMUSIC/plugins/bot/help.py
+++ DAXXMUSIC/plugins/bot/help.py
@@ -11,7 +11,7 @@ from strings import get_string, helpers
 from DAXXMUSIC.utils.stuffs.buttons import BUTTONS
 from DAXXMUSIC.utils.stuffs.helper import Helper
 
-@app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["ahelp"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
