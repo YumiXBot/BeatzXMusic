f85c973cb8a97b085fac9d28039291a401e2de64
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2024-02-07 17:07:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hny:zara@cluster0.lfe5o.mon
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1001603822916)
+LOGGER_ID = getenv("LOGGER_ID", "-1001603822916")
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6079943111))
